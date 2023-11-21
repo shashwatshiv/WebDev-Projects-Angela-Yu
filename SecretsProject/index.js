4621c1e39782fcd257b5f4fa4f6062a8be56d69f
Shashwat Shivhare <shashwat02infinity@gmail.com>
2023-11-21 20:18:28 +0530

    express js project

---
+++ SecretsProject/index.js
@@ -0,0 +1,25 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  console.log(req.body);
+  if (req.body.password === "ILoveProgramming") {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
+});
+app.listen(3000, console.log("port is up and running at " + port));
